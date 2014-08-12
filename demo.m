8135c5f43a7d41b1424df05f5a1b1695f80b0fd1
Stephen Tierney <sjtrny@gmail.com>
2014-08-12 11:49:31 +1000

    Code and docs upload

---
+++ demo.m
@@ -0,0 +1,23 @@
+paths = 'common:';
+addpath(paths);
+
+rng(1);
+
+rows = 100;
+n_space = 5;
+cluster_size = 50;
+
+A = rand(rows, n_space) * rand(n_space, n_space);
+
+permute_inds = reshape(repmat(1:n_space, cluster_size, 1), 1, n_space * cluster_size );
+A = A(:, permute_inds);
+
+corruption = 0;
+
+N = randn(size(A)) * corruption;
+
+X = A + N;
+
+A_est = rpca_fro(X, 0.01);
+
+rmpath(paths);
